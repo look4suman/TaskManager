0c7a3bd8cfe1258424a69f604cd3ff919be8f8c8
look4suman <look4suman@gmail.com>
2018-11-26 20:53:53 -0500

    added new components"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { ViewTaskComponent } from './view-task/view-task.component';
+import { UpdateTaskComponent } from './update-task/update-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTaskComponent,
+    ViewTaskComponent,
+    UpdateTaskComponent
   ],
   imports: [
     BrowserModule,
